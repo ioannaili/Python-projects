fa7ea3380dec24dd33b00dfe55f0e84a787644a7
ioannaili <47724538+ioannaili@users.noreply.github.com>
2021-02-10 16:08:10 +0200

    Update maxsub.py

--- ergasies/maxsub.py
+++ ergasies/maxsub.py
@@ -1,3 +1,8 @@
+"""
+This is a function called maxSequence that takes a list as input and returns the sublist (consecutive items in the list) that has the maximum sum.
+Example: maxSequence ([- 2, 1, -3, 4, -1, 2, 1, -5, 4]) returns 6: [4, -1, 2, 1]
+"""
+
 def maxSequence(a,size): 
       
     max_s = 0
